51dcf5481245b148bbcc9e3dd336f635687ef611
Jared Burkamper <jaredburkamper@gmail.com>
2021-08-28 18:53:58 -0500

    Slight changes

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,165 +1,169 @@
 // eslint-disable-next-line max-len
-import { PutItemInput, UpdateItemInput, DeleteItemInput, GetItemInput } from 'aws-sdk/clients/dynamodb';
+import {
+  PutItemInput,
+  UpdateItemInput,
+  DeleteItemInput,
+  GetItemInput,
+} from 'aws-sdk/clients/dynamodb';
 
 export interface IUser {
-    apiKey?: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    admin?: boolean;
+  apiKey?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  admin?: boolean;
 }
 
 class User implements IUser {
+  public apiKey?: string;
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  public admin?: boolean;
 
-    public apiKey?: string;
-    public firstName: string;
-    public lastName: string;
-    public email: string;
-    public admin?: boolean; 
+  // eslint-disable-next-line max-len
+  constructor(
+    firstName: string,
+    lastName: string,
+    email: string,
+    apiKey?: string,
+    admin?: boolean
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    if (apiKey) this.apiKey = apiKey;
+    if (admin) this.admin = admin;
+  }
 
-    // eslint-disable-next-line max-len
-    constructor(firstName: string, lastName: string, email: string, apiKey?: string, admin?: boolean) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        if (apiKey) this.apiKey = apiKey;
-        if (admin) this.admin = admin;
-    }
+  /**
+   * Creates the DynamoDB parameters for a put operation.
+   *
+   * @param apiKey
+   * @returns
+   */
+  public createSchema(apiKey: string): PutItemInput {
+    return {
+      Item: {
+        firstName: {
+          S: this.firstName,
+        },
+        lastName: {
+          S: this.lastName,
+        },
+        email: {
+          S: this.email,
+        },
+        admin: {
+          BOOL: false,
+        },
+        apiKey: {
+          S: apiKey,
+        },
+        orders: {
+          L: [],
+        },
+      },
+      TableName: 'Users',
+    };
+  }
 
-    
-    /**
-     * Creates the DynamoDB parameters for a put operation.
-     * 
-     * @param apiKey 
-     * @returns 
-     */
-    public createSchema(apiKey: string): PutItemInput {
-        return {
-            Item: {
-                "firstName": {
-                    S: this.firstName
-                },
-                "lastName": {
-                    S: this.lastName
-                },
-                "email": {
-                    S: this.email
-                },
-                "admin": {
-                    BOOL: false 
-                },
-                "apiKey": {
-                    S: apiKey
-                },
-                "orders": {
-                    L: []
-                }
-            },
-            TableName: "Users"
-        }
-    }
+  /**
+   * Creates the DynamoDB parameters for an update operation.
+   *
+   * @param apiKey
+   * @returns
+   */
+  public updateSchema(apiKey: string): UpdateItemInput {
+    return {
+      ExpressionAttributeNames: {
+        '#F': 'firstName',
+        '#L': 'lastName',
+        '#E': 'email',
+      },
+      ExpressionAttributeValues: {
+        ':f': { S: this.firstName },
+        ':l': { S: this.lastName },
+        ':e': { S: this.email },
+      },
+      Key: {
+        apiKey: { S: apiKey },
+      },
+      ReturnValues: 'ALL_NEW',
+      TableName: 'Users',
+      UpdateExpression: 'SET #F = :f, #L = :l, #E = :e',
+      ConditionExpression: 'attribute_exists(apiKey)',
+    };
+  }
 
+  /**
+   * Creates the DynamoDB parameters for a get operation.
+   *
+   * @param apiKey
+   * @returns
+   */
+  public static getSchema(apiKey: string): GetItemInput {
+    return {
+      TableName: 'Users',
+      Key: {
+        apiKey: {
+          S: apiKey,
+        },
+      },
+    };
+  }
 
-    /**
-     * Creates the DynamoDB parameters for an update operation.
-     * 
-     * @param apiKey 
-     * @returns 
-     */
-    public updateSchema(apiKey: string): UpdateItemInput {
-        return {
-            ExpressionAttributeNames: {
-                "#F": "firstName",
-                "#L": "lastName",
-                "#E": "email"
-            },
-            ExpressionAttributeValues: {
-                ":f": {S: this.firstName},
-                ":l": {S: this.lastName},
-                ":e": {S: this.email}
-            }, 
-            Key: {
-                "apiKey": {S: apiKey}
-            },
-            ReturnValues: "ALL_NEW",
-            TableName: "Users",
-            UpdateExpression: "SET #F = :f, #L = :l, #E = :e",
-            ConditionExpression: "attribute_exists(apiKey)"
-        }
-    }
+  /**
+   * Creates the DynamoDB parameters for a delete operation.
+   *
+   * @param apiKey
+   * @returns
+   */
+  public static removeSchema(apiKey: string): DeleteItemInput {
+    return {
+      TableName: 'Users',
+      Key: {
+        apiKey: {
+          S: apiKey,
+        },
+      },
+      ReturnValues: 'ALL_OLD',
+    };
+  }
 
+  /**
+   * Creates the DynamoDB parameters for an update operation.
+   *
+   * @param apiKey
+   * @returns
+   */
+  public static promoteSchema(apiKey: string): UpdateItemInput {
+    return {
+      ExpressionAttributeNames: {
+        '#a': 'admin',
+      },
+      ExpressionAttributeValues: {
+        ':a': { BOOL: true },
+      },
+      Key: {
+        apiKey: { S: apiKey },
+      },
+      ReturnValues: 'ALL_NEW',
+      TableName: 'Users',
+      UpdateExpression: 'SET #a = :a',
+      ConditionExpression: 'attribute_exists(apiKey)',
+    };
+  }
 
-    /**
-     * Creates the DynamoDB parameters for a get operation.
-     * 
-     * @param apiKey 
-     * @returns 
-     */
-    public static getSchema(apiKey: string): GetItemInput {
-        return {
-            TableName: "Users",
-            Key: {
-                "apiKey": {
-                    S: apiKey
-                }
-            }
-        }
-    }
-
-
-    /**
-     * Creates the DynamoDB parameters for a delete operation.
-     * 
-     * @param apiKey 
-     * @returns 
-     */
-    public static removeSchema(apiKey: string): DeleteItemInput {
-        return {
-            TableName: "Users",
-            Key: {
-                "apiKey": {
-                    S: apiKey
-                }
-            },
-            ReturnValues: "ALL_OLD"
-        }
-    }
-
-
-    /**
-     * Creates the DynamoDB parameters for an update operation.
-     * 
-     * @param apiKey 
-     * @returns 
-     */
-    public static promoteSchema(apiKey: string): UpdateItemInput {
-        return {
-            ExpressionAttributeNames: {
-                "#a": "admin"
-            },
-            ExpressionAttributeValues: {
-                ":a": {BOOL: true}
-            }, 
-            Key: {
-                "apiKey": {S: apiKey}
-            },
-            ReturnValues: "ALL_NEW",
-            TableName: "Users",
-            UpdateExpression: "SET #a = :a",
-            ConditionExpression: "attribute_exists(apiKey)"
-        }
-    }
-
-
-    /**
-     * Creates a User object from an untyped object.
-     * 
-     * @param obj 
-     * @returns 
-     */
-    public static createFromObject(obj: any): User {
-        return new User(obj.firstName, obj.lastName, obj.email);
-    }
+  /**
+   * Creates a User object from an untyped object.
+   *
+   * @param obj
+   * @returns
+   */
+  public static createFromObject(obj: any): User {
+    return new User(obj.firstName, obj.lastName, obj.email, obj.apiKey);
+  }
 }
 
 export default User;
--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -9,86 +9,80 @@ import { paramMissingError } from '@shared/constants';
 const userDao = new UserDao();
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
-
-
 /**
  * Get a user.
- * 
- * @param req 
- * @param res 
- * @returns 
+ *
+ * @param req
+ * @param res
+ * @returns
  */
 export async function getUser(req: Request, res: Response) {
-    const { apiKey } = req.params;
-    const response = await userDao.get(apiKey);
-    res.status(OK).json(response);
+  const { apiKey } = req.params;
+  const response = await userDao.get(apiKey);
+  res.status(OK).json(response);
 }
-
 
 /**
  * Add a user.
- * 
- * @param req 
- * @param res 
- * @returns 
+ *
+ * @param req
+ * @param res
+ * @returns
  */
 export async function createUser(req: Request, res: Response) {
-    if (!req.body.user) {
-        return res.status(BAD_REQUEST).json({
-            error: paramMissingError
-        });
-    }
-    const user = User.createFromObject(req.body.user)
-    const response = await userDao.create(user);
-    return res.status(CREATED).json(response);
+  if (!req.body.user) {
+    return res.status(BAD_REQUEST).json({
+      error: paramMissingError,
+    });
+  }
+  const user = User.createFromObject(req.body.user);
+  const response = await userDao.create(user);
+  return res.status(CREATED).json(response);
 }
-
 
 /**
  * Update a user.
- * 
- * @param req 
- * @param res 
- * @returns 
+ *
+ * @param req
+ * @param res
+ * @returns
  */
 export async function updateUser(req: Request, res: Response) {
-    if (!req.body.user || !req.body.apiKey) {
-        return res.status(BAD_REQUEST).json({
-            error: paramMissingError,
-        });
-    }
-    const user = User.createFromObject(req.body.user);
-    const response = await userDao.update(req.body.apiKey, user);
-    return res.status(OK).json(response);
+  if (!req.body.user || !req.body.user.apiKey) {
+    return res.status(BAD_REQUEST).json({
+      error: paramMissingError,
+    });
+  }
+  const user = User.createFromObject(req.body.user);
+  const response = await userDao.update(req.body.user.apiKey, user);
+  return res.status(OK).json(response);
 }
-
 
 /**
  * Delete a user.
- * 
- * @param req 
- * @param res 
- * @returns 
+ *
+ * @param req
+ * @param res
+ * @returns
  */
 export async function removeUser(req: Request, res: Response) {
-    const { apiKey } = req.params;
-    const response = await userDao.remove(apiKey);
-    return res.status(OK).json(response);
+  const { apiKey } = req.params;
+  const response = await userDao.remove(apiKey);
+  return res.status(OK).json(response);
 }
-
 
 /**
  * Promote a user.
- * 
- * @param req 
- * @param res 
+ *
+ * @param req
+ * @param res
  */
 export async function promoteUser(req: Request, res: Response) {
-    if (!req.body.apiKey || !req.body.promoteKey) {
-        return res.status(BAD_REQUEST).json({
-            error: paramMissingError
-        });
-    }
-    const response = await userDao.promote(req.body.apiKey, req.body.promoteKey)
-    return res.status(OK).json(response);
+  if (!req.body.apiKey || !req.body.promoteKey) {
+    return res.status(BAD_REQUEST).json({
+      error: paramMissingError,
+    });
+  }
+  const response = await userDao.promote(req.body.apiKey, req.body.promoteKey);
+  return res.status(OK).json(response);
 }
